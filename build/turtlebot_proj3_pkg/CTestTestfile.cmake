ef3001270a4ec69de0008f067b760000526214b0
Class Account <ee106b-abv@c105-7.local>
2025-03-15 20:33:23 -0700

    uni(dr/kf)&quad(dr) finished

--- build/turtlebot_proj3_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/ee106b/sp25/class/ee106b-abv/project-3-name/src/turtlebot_proj3_pkg
-# Build directory: /home/cc/ee106b/sp25/class/ee106b-abv/project-3-name/build/turtlebot_proj3_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
